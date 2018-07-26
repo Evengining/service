436e55b94256d0921fb9e75acf560a8720589df2
evgeny <evengining@yandex.ru>
2018-07-26 14:40:11 +0300

    Edit verification

--- server/src/index.php
+++ server/src/index.php
@@ -3,46 +3,36 @@ include "connectionToQueue/connect.php";
 
 $data = isset($_GET['data']) ? json_decode($_GET['data'], true) : exit('Not found get parameters');
 
+try {
+    if(!is_array($data))
+        throw new Exception('Error int parameters.');
 
-function verification(array $data) {
-    if(count(array_intersect_key(array_flip(['x', 'y', 'width', 'height', 'return_url','path']), $data)) === 6) {
-        foreach ($data as $key => $value) {
-            if($key !== "return_url" && $key !== "path") {
-                if(!is_int($value))
-                    return "Crop parameters type is not integer";
-            } else if($key === "path") {
-                if(!is_string($value)) {
-                    return "Image path is not string";
-                } else {
-                    $returnes = @get_headers($value);
-                    if(!strpos($returnes[0], '200')) {
-                        return "Broken path";
-                    } else {
-                        if(!strpos($value, '.jpg') && !strpos($value, '.png') && !strpos($value, '.jpeg')) {
-                            return "Image not found";
-                        }
-                    }
-                }
-            } else {
-                if(!is_string($value))
-                    return "Url is not string";
-            }
-        }
-        return true;
-    } else {
-        return "Error in parameters";
-    }
-}
+    else if(count(array_intersect_key(array_flip(['x', 'y', 'width', 'height', 'return_url', 'path']), $data)) !== 6)
+        throw new Exception('Error in parameters.');
 
-if(is_array($data)) {
-   if(is_string($res = verification($data))) {
-       echo $res;
-   } else {
-       $queue->publish('task', $_GET['data']);
-       echo "OK!";
-   }
-} else {
-    echo "Error in parameters";
+
+    else if(!is_int($data['x']) || !is_int($data['y']) || !is_int($data['width']) || !is_int($data['height']))
+        throw new Exception('Crop parameters type is not integer.');
+
+
+    else if(!is_string($data['path']) || !is_string($data['return_url']))
+        throw new Exception('Return_url or path parameters type is not string.');
+
+
+    $returnes = @get_headers($data['path']);
+
+    if(!strpos($returnes[0], '200'))
+        throw new Exception('Broken path');
+
+
+    else if(!strpos($data['path'], '.jpg') && !strpos($data['path'], '.png') && !strpos($data['path'], '.jpeg'))
+        throw new Exception('Image not found');
+
+
+    $queue->publish('task', $_GET['data']);
+    echo "OK!";
+} catch (Exception $e) {
+    echo $e->getMessage();
 }
 
 
