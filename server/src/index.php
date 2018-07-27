a219c4e28dbfe0acbbbb6ca3bcd0a68efa6b64cf
evgeny <evengining@yandex.ru>
2018-07-27 04:24:39 +0300

    Refactor validators data

--- server/src/index.php
+++ server/src/index.php
@@ -1,38 +1,28 @@
 <?php
+declare(strict_types=1);
 include "connectionToQueue/connect.php";
 
 $data = isset($_GET['data']) ? json_decode($_GET['data'], true) : exit('Not found get parameters');
 
-try {
-    if(!is_array($data))
-        throw new Exception('Error int parameters.');
+function validate(int $x, int $y, int $width, int $height, string $return_url, string $path) {
+    $returnes = @get_headers($path);
 
-    else if(count(array_intersect_key(array_flip(['x', 'y', 'width', 'height', 'return_url', 'path']), $data)) !== 6)
-        throw new Exception('Error in parameters.');
-
-
-    else if(!is_int($data['x']) || !is_int($data['y']) || !is_int($data['width']) || !is_int($data['height']))
-        throw new Exception('Crop parameters type is not integer.');
-
-
-    else if(!is_string($data['path']) || !is_string($data['return_url']))
-        throw new Exception('Return_url or path parameters type is not string.');
-
-
-    $returnes = @get_headers($data['path']);
-
-    if(!strpos($returnes[0], '200'))
+    if(!strpos($returnes[0] ?? 'false', '200'))
         throw new Exception('Broken path');
 
 
-    else if(!strpos($data['path'], '.jpg') && !strpos($data['path'], '.png') && !strpos($data['path'], '.jpeg'))
+    if(!strpos($path, '.jpg') && !strpos($path, '.png') && !strpos($path, '.jpeg'))
         throw new Exception('Image not found');
+}
 
-
+try {
+    validate($data['x'], $data['y'], $data['width'], $data['height'], $data['return_url'], $data['path']);
     $queue->publish('task', $_GET['data']);
     echo "OK!";
 } catch (Exception $e) {
     echo $e->getMessage();
+} catch (TypeError $e) {
+    echo "Error in parameters.";
 }
 
 
